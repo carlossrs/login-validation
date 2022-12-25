cde221823c9a86e21be3d61646c95c14102440f0
Carlos Sá Ribas Santos <carlos.saribas@gmail.com>
2022-12-25 20:26:36 -0300

    first commit

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  width: 100%;
+  height: 42px;
+  background-color: #270b2f;
+  color: #fff;
+  border: 1px solid #270b2f;
+  border-radius: 21px;
+  &:hover {
+    opacity: 0.6;
+    cursor: pointer;
+  }
+`;
---
+++ src/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: 'Nunito', sans-serif;
+        box-sizing:  border-box;
+    }
+    body{
+      background-color:#270b2f;
+    }
+`;
